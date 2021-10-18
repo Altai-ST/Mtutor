0f314011a6556b92725c574882421e2e305902d0
Altai <altaistamov2003@gmail.com>
2021-10-18 17:43:14 +0600

    test-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
+import Register from "./components/Register/register";
+import Catalog from "./components/Catalog";
 function App() {
   return (
     <div className="App">
+      <Catalog/>
     </div>
   );
 }
---
+++ src/components/Catalog/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Stack} from "react-bootstrap";
+
+const Catalog =()=>{
+    return(
+        <Stack>
+            
+        </Stack>
+    )
+}
+
+export default Catalog
