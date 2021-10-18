2abcb809d317f57017d7369c75b4565096f26679
Altai <altaistamov2003@gmail.com>
2021-10-18 19:43:40 +0600

    add verstka Catalog-Reg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Register from "./components/Register/register";
 import Catalog from "./components/Catalog";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import { Stack} from "react-bootstrap";
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import styles from './style.module.css'
+
 
 const Catalog =()=>{
     return(
-        <Stack>
-            
-        </Stack>
+        <div>
+            <Container className='mt-5'>
+                <Col className={styles.col1}>
+                    <Col className={styles.row1}>
+                        <Row>
+                            <h4 className='mt-4 pt-1'> Добро пожаловать в Mtutor!!!</h4>
+                        </Row>
+                        <Row>
+                            <h6>Расскажи нам о себе</h6>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <h1>Я</h1>
+                        </Row>
+                        <Row className='mb-2 px-2'>
+                            <button className={styles.btnViolet +' '+ 'py-2'}>
+                                Студент
+                            </button>
+                        </Row>
+                        <Row className='px-2'>
+                            <button className={styles.btnViolet +' '+ 'py-2'}>Репетитор</button>
+                        </Row>
+                    </Col>
+                </Col>
+                
+            </Container>
+        </div>
     )
 }
 
