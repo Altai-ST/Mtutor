79f0da95f3f596da87922c2652c0e51bf64a90ba
Altai <altaistamov2003@gmail.com>
2021-10-27 14:39:13 +0600

    add icons and correct modal window

--- src/components/LoginContent/index.jsx
+++ src/components/LoginContent/index.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import { Button, Form } from "react-bootstrap";
 import LoginModal from "../LoginModal";
-
+import { FaUserAlt, FaLock } from "react-icons/fa"
+import style from './loginContent.module.scss'
 
 export const LoginContent = ()=>{
     const [show, setShow] = useState(false);
@@ -16,18 +17,18 @@ export const LoginContent = ()=>{
             <LoginModal show={show} handleClose={handleClose} onClose={true} title={title} >
                 <Form>
                     <Form.Group>
-                        <div>
-                            <Form.Label >icon</Form.Label>
+                        <div className={style.userAlt}>
+                            <Form.Label className={style.icon}><FaUserAlt></FaUserAlt></Form.Label>
                             <div>
-                                <Form.Control type="fullName" placeholder="Email"></Form.Control>
+                                <Form.Control className={style.emailForm} type="fullName" placeholder="Email"></Form.Control>
                             </div>
                         </div>
                     </Form.Group>
                     <Form.Group>
-                        <div>
-                            <Form.Label >icon</Form.Label>
+                        <div className={style.lock}>
+                            <Form.Label className={style.icon}><FaLock></FaLock></Form.Label>
                             <div>
-                                <Form.Control type="fullName" placeholder="Password"></Form.Control>
+                                <Form.Control className={style.emailForm} type="fullName" placeholder="Password"></Form.Control>
                             </div>
                         </div>
                     </Form.Group>
--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -7,23 +7,23 @@ const LoginModal=({show, handleClose, children, onClose, title})=>{
         <div>
             <Modal
             show={show}
-            size="lg"
+            size="md"
             aria-labelledby="contained-modal-title-vcenter"
             centered
             >
-                <Modal.Header>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        {onClose ? <h5>{title}</h5> : ''}
-                    </Modal.Title>
-                    {onClose ? <button onClick={handleClose} className='btn-close' aria-label='Close'></button> : ''}
-                </Modal.Header>
-                <Modal.Body>
-                    {children}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={handleClose}>Войти</Button>
-                    <Button onClick={handleClose}>Отмена</Button>
-                </Modal.Footer>
+                    <Modal.Header>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            {onClose ? <h5>{title}</h5> : ''}
+                        </Modal.Title>
+                        {onClose ? <button onClick={handleClose} className='btn-close' aria-label='Close'></button> : ''}
+                    </Modal.Header>
+                    <Modal.Body>
+                        {children}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={handleClose}>Войти</Button>
+                        <Button onClick={handleClose}>Отмена</Button>
+                    </Modal.Footer>
             </Modal>
         </div>
     )
