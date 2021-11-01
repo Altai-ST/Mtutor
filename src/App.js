01c484a328b8e2d06b284e7438c99253cac80bd5
munkg92 <Nurperi-Mavlianbekkyzy.com>
2021-11-01 15:55:23 +0600

    n/d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./assets/styles/App.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routers } from "./components/Routers";
 
 
 function App() {
   return (
     <div className="App">
+      <Routers/>
     </div>
   );
 }
