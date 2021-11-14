84f56943ffd9b8ecd2d6b96a1c80bbb0eb816921
munkg92 <Nurperi-Mavlianbekkyzy.com>
2021-11-14 16:34:23 +0600

    nd

--- src/components/Admin/TutorApplication/index.jsx
+++ src/components/Admin/TutorApplication/index.jsx
@@ -8,14 +8,9 @@ import { useDispatch } from 'react-redux'
 
 const TutorApplication = () => {
 	const dispatch = useDispatch()
-	function func() {
-		fetch('http://ec2-18-184-251-15.eu-central-1.compute.amazonaws.com:8000/user/applicants?status=pending',{
-			method: 'GET',
-			headers: {Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6I…DI4fQ.X41P9jbfcKA5A5FJEV0qSKB1IYDs1H-JiA-0AoESfK4'}
-		}).then(res => res.json).then(response => console.log('response =>', response)).catch(err => console.log(err))
-	}
+	
 	useEffect(() => {
-		func()
+		dispatch(getapplication())
 	},[])
 	return (
 		<div>
